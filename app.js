120fc990661534f2810e0542cf06b3257ffd791d
Rishav Shah <99394765+Rishav840@users.noreply.github.com>
2024-04-05 14:21:13 +0530

    Update the apiKey

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const apiKey="efdab26e5b7b5e255fb4c7a685e85b0e";
+const apiKey="apikey";
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
